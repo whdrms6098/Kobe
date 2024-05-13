2022a4b8c58a07e0bcd3567f08dbaeda79c3c0bd
whdrms6098 <whdrms6098@naver.com>
2024-05-14 01:04:45 +0900

    visitor 기본 뷰

--- js/common.js
+++ js/common.js
@@ -9,6 +9,10 @@ function headerBg() {
 
 setInterval(headerBg, 5000);
 
+$("#to-top").click(function () {
+  gsap.to(window, { duration: 0.7, scrollTo: 0 });
+});
+
 $(".menu-open").click(function (e) {
   e.stopPropagation();
   $(".side-menu").fadeIn("fast");
@@ -19,34 +23,4 @@ $(".side-menu-close").click(function (e) {
   e.stopPropagation();
   $(".side-menu").fadeOut("fast");
   $("body").css("overflow", "auto");
-});
-
-$(".menu-subject-open").click(function (e) {
-  e.stopPropagation();
-  $(this).css("display", "none");
-  $(".menu-subject-close").addClass("active");
-  $(".sub-menu-subject").show();
-});
-
-$(".menu-subject-close").click(function (e) {
-  e.stopPropagation();
-  $(this).removeClass("active");
-  $(".menu-subject-open").show();
-  $(".sub-menu-subject").hide();
-});
-
-$(".subject-menu").click(function (e) {
-  e.stopPropagation();
-});
-
-$(window).click(function () {
-  if ($(window).width() <= 980) {
-    $(".menu-subject-close").removeClass("active");
-    $(".menu-subject-open").show();
-    $(".sub-menu-subject").hide();
-  }
-});
-
-$("#to-top").click(function () {
-  gsap.to(window, { duration: 0.7, scrollTo: 0 });
 });
--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+$(".menu-subject-open").click(function (e) {
+  e.stopPropagation();
+  $(this).css("display", "none");
+  $(".menu-subject-close").addClass("active");
+  $(".sub-menu-subject").show();
+});
+
+$(".menu-subject-close").click(function (e) {
+  e.stopPropagation();
+  $(this).removeClass("active");
+  $(".menu-subject-open").show();
+  $(".sub-menu-subject").hide();
+});
+
+$(".subject-menu").click(function (e) {
+  e.stopPropagation();
+});
+
+$(window).click(function () {
+  if ($(window).width() <= 980) {
+    $(".menu-subject-close").removeClass("active");
+    $(".menu-subject-open").show();
+    $(".sub-menu-subject").hide();
+  }
+});
+
 new Swiper(".banner .swiper-container", {
   spaceBetween: 40,
   freeMode: false,
